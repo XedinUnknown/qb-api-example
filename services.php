015d99f0168d8bdb4b1b80df3eb630161a311914
Anton Ukhanev <xedin.unknown@gmail.com>
2018-09-20 19:58:58 +0200

    Added configuration and an oAuth provider service

--- services.php
+++ services.php
@@ -1,7 +1,32 @@
 <?php
 
+use League\OAuth2\Client\Provider\GenericProvider;
+use XedinUnknown\QbApiExample\Auth_Handler;
+use XedinUnknown\QbApiExample\DI_Container;
+
 return function ($rootPath) {
     return [
+        'base_dir' => $rootPath,
+        'base_url' => 'http://wpra-php-70.local/qb',
+        'access_token_file_name' => 'access_token',
 
+        'client_id' => '',
+        'client_secret' => '',
+
+        'oauth_provider' => function ( DI_Container $c ) {
+            return new GenericProvider([
+                'clientId'                  => $c->get('client_id'),    // The client ID assigned to you by the provider
+                'clientSecret'              => $c->get('client_secret'),   // The client password assigned to you by the provider
+                'redirectUri'               => 'http://wpra-php-70.local/qb/auth.php',
+                'urlAuthorize'              => 'https://appcenter.intuit.com/connect/oauth2',
+                'urlAccessToken'            => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
+                'urlResourceOwnerDetails'   => 'http://brentertainment.com/oauth2/lockdin/resource',
+                'scopes'	                => 'com.intuit.quickbooks.accounting'
+            ]);
+        },
+
+        'auth_handler' => function ( DI_Container $c ) {
+            return new Auth_Handler( $c );
+        }
     ];
 };
---
+++ src/class-auth-handler.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Auth_Handler class.
+ *
+ * @package QbApiExample
+ */
+
+namespace XedinUnknown\QbApiExample;
+
+/**
+ * Handles authentication.
+ *
+ * @package QbApiExample
+ */
+class Auth_Handler extends Handler {
+
+    public function hook() {
+        $script = basename($_SERVER['SCRIPT_FILENAME']);
+
+        if($script === 'index.php') {
+            $this->main_page();
+        }
+        if($script === 'auth.php') {
+            $this->auth_page();
+        }
+    }
+
+    protected function main_page() {
+        if(!($token = $this->get_token())) {
+            header(vsprintf('Location: %1$s/auth.php', [$this->get_config('base_url')]));
+            die();
+        }
+    }
+
+
+    protected function auth_page() {
+        $provider = $this->get_config('oauth_provider');
+
+        // First time on this page - no code passed from QB API
+        if (!isset($_GET['code'])) {
+            /* @var $provider \League\OAuth2\Client\Provider\GenericProvider */
+            $redirectUrl = $provider->getAuthorizationUrl();
+            $_SESSION['oauth2state'] = $provider->getState();
+            header(vsprintf('Location: %1$s', [$redirectUrl]));
+            die();
+        }
+
+        // Checking integrity of data passed from QB API after redirect
+        if (!isset($_SESSION['oauth2state']) || !isset($_GET['state']) || $_SESSION['oauth2state'] !== $_GET['state']) {
+            echo 'Security problem';
+            die();
+        }
+
+        unset($_SESSION['oauth2state']);
+        $accessToken = $provider->getAccessToken('authorization_code', [
+            'code' => $_GET['code']
+        ]);
+
+        $this->save_token($accessToken);
+        header(vsprintf('Location: %1$s', [$this->get_url()]));
+
+        return;
+    }
+
+
+    public function get_token() {
+        return file_get_contents($this->get_token_file_path());
+    }
+
+    /**
+     * Saves token.
+     *
+     * @param string $token Token to save
+     */
+    protected function save_token($token) {
+        file_put_contents($this->get_token_file_path(), $token);
+    }
+
+    /**
+     * @return string The path to the token file.
+     */
+    protected function get_token_file_path() {
+        $rootPath = rtrim($this->get_config('base_dir'), '/');
+        $tokenFile = ltrim($this->get_config('access_token_file_name'), '/' );
+        $tokenFilePath = "$rootPath/$tokenFile";
+
+        return $tokenFilePath;
+    }
+
+}
