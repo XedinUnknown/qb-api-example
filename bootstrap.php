ac4fa225d02814b3fa63c38cd85753cc5d37ea12
Anton Ukhanev <xedin.unknown@gmail.com>
2018-09-20 19:57:31 +0200

    Bootstrap now starts session

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,12 @@ if (file_exists($autoload = 'vendor/autoload.php')) {
     require_once $autoload;
 }
 
+$session_id = session_id();
+if (empty($session_id))
+{
+    session_start();
+}
+
 $serviceFactory = require_once('services.php');
 $services = $serviceFactory(__DIR__);
+$c = new \XedinUnknown\QbApiExample\DI_Container($services);
